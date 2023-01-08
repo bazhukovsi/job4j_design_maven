23e84510af919f5685a669f9823e80b7d6ac7598
bazhukovsi <bazhukovsi@yandex.ru>
2023-01-08 20:53:59 +0300

    2. Триггеры [#504803]

---
+++ script/trigger_products.sql
@@ -0,0 +1,61 @@
+create database products;
+
+create table products (
+                          id serial primary key,
+                          name varchar(50),
+                          producer varchar(50),
+                          count integer default 0,
+                          price integer
+);
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 8, 40);
+insert into products (name, producer, count, price) VALUES ('product_2', 'producer_2', 10, 50);
+insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 12, 60);
+insert into products (name, producer, count, price) VALUES ('product_4', 'producer_4', 14, 70);
+insert into products (name, producer, count, price) VALUES ('product_5', 'producer_5', 16, 80);
+insert into products (name, producer, count, price) VALUES ('product_6', 'producer_6', 18, 90);
+insert into products (name, producer, count, price) VALUES ('product_7', 'producer_7', 20, 100);
+
+create or replace function tax()
+    returns trigger as
+$$
+BEGIN
+    update products
+    set price = price + price * 0.2;
+    return new;
+END;
+$$
+    LANGUAGE 'plpgsql';
+
+create trigger tax_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+execute procedure tax();
+
+insert into products (name, producer, count, price) VALUES ('product_8', 'producer_8', 22, 110);
+
+create or replace function history_write()
+    returns trigger as
+$$
+    BEGIN
+        insert history_of_price
+        (name, price, date) values
+        ('products', 12.88, '2023-01-01 00:00:01');
+         return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger history_price
+    after insert
+    on products
+    for each row
+    execute procedure history_write();
