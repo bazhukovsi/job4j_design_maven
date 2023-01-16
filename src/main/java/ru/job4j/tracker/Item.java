d884c3bde25549a0a6167fb1d90c46e20e7f45be
bazhukovsi <bazhukovsi@yandex.ru>
2023-01-16 14:21:29 +0300

    2. @Setter, @Getter и другие [#504889 #381686] Update

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,13 +1,21 @@
 package ru.job4j.tracker;
 
-import lombok.Data;
+import lombok.*;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
 public class Item  {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    @Getter
+    @NonNull
+    @EqualsAndHashCode.Include
     private int id;
+    @Getter
+    @Setter
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 }
--- src/main/java/ru/job4j/tracker/LombokUsageItem.java
+++ src/main/java/ru/job4j/tracker/LombokUsageItem.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 public class LombokUsageItem {
     public static void main(String[] args) {
-        var item = new Item();
+        var item = new Item(1);
         item.setName("Work");
         System.out.println(item);
     }
