b6882fd2eb9d0a48fe6f57cd47e321f095a0be2a
bazhukovsi <bazhukovsi@yandex.ru>
2023-01-16 17:59:15 +0300

    3. @Builder [#504890]

--- src/main/java/ru/job4j/lombok/LombokUsage.java
+++ src/main/java/ru/job4j/lombok/LombokUsage.java
@@ -2,8 +2,16 @@ package ru.job4j.lombok;
 
 public class LombokUsage {
     public static void main(String[] args) {
-        var bird = new BirdData();
-        bird.setAge(1);
-        System.out.println(bird);
+        Permission permission = Permission.of()
+                .id(1)
+                .name("ADMIN")
+                .accessBy("read")
+                .accessBy("write")
+                .accessBy("execute")
+                .accessBy("read, write")
+                .accessBy("read, execute")
+                .accessBy("read, write, execute")
+                .build();
+        System.out.println(permission);
     }
 }
